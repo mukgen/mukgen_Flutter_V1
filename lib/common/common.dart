d7eeb5791f0d9a664a722ac322491d17ea2fa7ba
yoochanhong <chanhongy6@naver.com>
2023-05-20 13:45:44 +0900

    ✨ :: 커스텀 색상 파일 추가

---
+++ lib/common/common.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MukGenColor {
+  //색깔 쓸때 이거 쓰며 ㄴ돼!
+  //자주 쓸 색깔이 생기면 추가해줘
+  static Color pointBase = const Color(0xffFE6B01);
+  static Color pointLight1 = const Color(0xffFF7A1B);
+  static Color pointLight2 = const Color(0xffFE984D);
+  static Color pointLight3 = const Color(0xffFEB580);
+  static Color pointLight4 = const Color(0xffFFE1CC);
+
+  static Color grey = const Color(0xFF6B6C71);
+  static Color black = const Color(0xff000000);
+  static Color white = const Color(0xFFFFFFFF);
+
+  static Color postIt1 = const Color(0xFFFFE8BC);
+  static Color red = const Color(0xFFD52020);
+  static Color red2 = const Color(0xFFFFDCDC);
+  static Color green = const Color(0xFF2FA225);
+  static Color green2 = const Color(0xFFD6FFD2);
+
+  static Color primaryBase = const Color(0xFF6B6C71);
+  static Color primaryLight1 = const Color(0xFF85858D);
+  static Color primaryLight2 = const Color(0xFFC9CAD0);
+  static Color primaryLight3 = const Color(0xFFF5F6F6);
+  static Color primaryDark1 = const Color(0xFF494A4F);
+  static Color primaryDark2 = const Color(0xFF333438);
+}
