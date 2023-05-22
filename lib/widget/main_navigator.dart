ab26f22d2bcdb27c82016734d49223c429d64f2f
yj77iw0 <treeyu12345@naver.com>
2023-05-22 19:07:02 +0900

    ⚡️ ::  하위 페이지에 들어가도 bottomNavigationBar가 뜨게 변경
    하위 페이지, 원래 있던 페이지에서 다른 페이지로 이동했다가 다시 와도 활동하던 데이터 유지, WillPopScope 적용.

--- lib/widget/main_navigator.dart
+++ lib/widget/main_navigator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mukgen_flutter_v1/common/common.dart';
+import 'package:mukgen_flutter_v1/screen/main_review_otherdays.dart';
 import 'package:mukgen_flutter_v1/widget/custom_icons.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mukgen_flutter_v1/screen/main_page.dart';
@@ -7,6 +8,7 @@ import 'package:mukgen_flutter_v1/screen/main_board.dart';
 import 'package:mukgen_flutter_v1/screen/main_delivery_party.dart';
 import 'package:mukgen_flutter_v1/screen/main_review.dart';
 import 'package:mukgen_flutter_v1/screen/main_suggestion.dart';
+import 'package:flutter/widgets.dart';
 
 class MainNavigator extends StatefulWidget {
   const MainNavigator({Key? key}) : super(key: key);
@@ -18,93 +20,121 @@ class MainNavigator extends StatefulWidget {
 class _MainNavigatorState extends State<MainNavigator> {
   int _selectedIndex = 0;
 
-  final List<Widget> _pages = [
-    const MainHomePage(),
-    const MainBoardPage(),
-    const MainSuggestionPage(),
-    const MainReviewPage(),
-    const MainDeliveryPartyPage(),
+  List<GlobalKey<NavigatorState>> _navigatorKeys = [
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>()
   ];
-
-  final PageController _pageController = PageController(initialPage: 0);
-
-  void _onPageChanged(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  void _onItemTapped(int index) {
-    if (_selectedIndex != index) {
-      setState(() {
-        _selectedIndex = index;
-        _pageController.animateToPage(
-          index,
-          duration: const Duration(milliseconds: 400),
-          curve: Curves.ease,
-        );
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: _onPageChanged,
-        children: _pages,
-      ),
-      bottomNavigationBar: SizedBox(
-        height: 86.0.h,
-        child: BottomNavigationBar(
-          backgroundColor: MukGenColor.white,
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(CustomIcons.home),
-              label: '홈',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CustomIcons.board),
-              label: '게시판',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CustomIcons.vector),
-              label: '급식 건의',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.star),
-              label: '급식 리뷰',
-            ),
-            BottomNavigationBarItem(
-              icon: Column(
-                children: [
-                  Icon(Icons.groups),
-                ],
-              ),
-              label: '배달 파티',
-            ),
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab =
+            !await _navigatorKeys[_selectedIndex].currentState!.maybePop();
+
+        print(
+            'isFirstRouteInCurrentTab: ' + isFirstRouteInCurrentTab.toString());
+
+        return isFirstRouteInCurrentTab;
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            _buildOffstageNavigator(0),
+            _buildOffstageNavigator(1),
+            _buildOffstageNavigator(2),
+            _buildOffstageNavigator(3),
+            _buildOffstageNavigator(4),
           ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-          unselectedLabelStyle: TextStyle(
-            color: MukGenColor.primaryLight2,
-            fontWeight: FontWeight.w600,
-            fontSize: 14,
-            fontFamily: 'MukgenSemiBold',
-          ),
-          selectedLabelStyle: TextStyle(
-            color: MukGenColor.pointBase,
-            fontWeight: FontWeight.w600,
-            fontSize: 14,
-            fontFamily: 'MukgenSemiBold',
-          ),
-          selectedItemColor: MukGenColor.pointBase,
-          unselectedItemColor: MukGenColor.primaryLight2,
-          iconSize: 30,
-          elevation: 0,
         ),
+        bottomNavigationBar: SizedBox(
+          height: 86.0.h,
+          child: BottomNavigationBar(
+            backgroundColor: MukGenColor.white,
+            type: BottomNavigationBarType.fixed,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(CustomIcons.home),
+                label: '홈',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CustomIcons.board),
+                label: '게시판',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CustomIcons.vector),
+                label: '급식 건의',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.star),
+                label: '급식 리뷰',
+              ),
+              BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    Icon(Icons.groups),
+                  ],
+                ),
+                label: '배달 파티',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            unselectedLabelStyle: TextStyle(
+              color: MukGenColor.primaryLight2,
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+              fontFamily: 'MukgenSemiBold',
+            ),
+            selectedLabelStyle: TextStyle(
+              color: MukGenColor.pointBase,
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+              fontFamily: 'MukgenSemiBold',
+            ),
+            selectedItemColor: MukGenColor.pointBase,
+            unselectedItemColor: MukGenColor.primaryLight2,
+            iconSize: 30,
+            elevation: 0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Map<String, WidgetBuilder> _routeBuilders(BuildContext context, int index) {
+    return {
+      '/': (context) {
+        return [
+          const MainHomePage(),
+          const MainBoardPage(),
+          const MainSuggestionPage(),
+          const MainReviewPage(),
+          const MainDeliveryPartyPage(),
+        ].elementAt(index);
+      },
+    };
+  }
+
+  Widget _buildOffstageNavigator(int index) {
+    var routeBuilders = _routeBuilders(context, index);
+
+    return Offstage(
+      offstage: _selectedIndex != index,
+      child: Navigator(
+        key: _navigatorKeys[index],
+        onGenerateRoute: (routeSettings) {
+          return MaterialPageRoute(
+            builder: (context) => routeBuilders[routeSettings.name]!(context),
+          );
+        },
       ),
     );
   }
