4089407828ed504cddf6fac78ce95bcde5aa3a11
yj77iw0 <treeyu12345@naver.com>
2023-06-02 23:18:59 +0900

    ✨ :: 일반인 회원가입 서버 연동 함수 작성
    서버 연동 핸

---
+++ lib/service/post_general_signup_info.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mukgen_flutter_v1/secret.dart';
+
+Future<int> postGeneralSignup(String name, String accountId, String password, String passwordCheck, String phoneNumber) async {
+
+  Map data = {"name": name,
+              "accountId" : accountId,
+              "password" : password,
+              "passwordCheck" : passwordCheck,
+              "phoneNumber" : phoneNumber };
+
+  var body = json.encode(data);
+
+  final response = await http.post(
+    Uri.parse("$baseUrl/auth/signup/general"),
+    headers: {"Content-Type": "application/json",},
+    body: body);
+  if (response.statusCode == 200) {
+    return 1;
+  } else{
+    print(utf8.decode(response.bodyBytes));
+    return 0;
+  }
+}
