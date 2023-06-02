9c6a646f1bba9fc84db7fc025c9bb751c3457102
yj77iw0 <treeyu12345@naver.com>
2023-06-02 23:25:13 +0900

    ⚡️ :: 배달 파티 모집 페이지로 가기 위한 버튼 제작
    제작 했샴

--- lib/screen/main_delivery_party.dart
+++ lib/screen/main_delivery_party.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mukgen_flutter_v1/common/common.dart';
+import 'package:mukgen_flutter_v1/screen/delivery/delivery_what_food.dart';
+import 'package:transition/transition.dart';
 
 class MainDeliveryPartyPage extends StatefulWidget {
   const MainDeliveryPartyPage({Key? key}) : super(key: key);
@@ -10,6 +14,29 @@ class MainDeliveryPartyPage extends StatefulWidget {
 class _MainDeliveryPartyPageState extends State<MainDeliveryPartyPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: MukGenColor.white,
+      floatingActionButton: SizedBox(
+        width: 70.0.w,
+        height: 70.0.h,
+        child: FittedBox(
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                Transition(
+                  child: DeliveryWhatFoodPage(),
+                  transitionEffect: TransitionEffect.RIGHT_TO_LEFT,
+                ),
+              );
+            },
+            elevation: 0,
+            backgroundColor: MukGenColor.pointBase,
+            child: Icon(Icons.add, size: 30.0.sp, color: MukGenColor.white),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 }
