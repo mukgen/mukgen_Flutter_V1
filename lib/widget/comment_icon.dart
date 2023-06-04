b263b7a3cf83d942723b255f2d03381a6dd2e01a
yoochanhong <chanhongy6@naver.com>
2023-06-05 08:24:58 +0900

    ✨ :: 커스텀 아이콘 추가

---
+++ lib/widget/comment_icon.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class CommentIcon {
+  CommentIcon._();
+
+  static const _kFontFam = 'CommentIcon';
+  static const String? _kFontPkg = null;
+
+  static const IconData commentCount =
+      IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
