05867ce35b6dbfe1ab9bdbd48e15f2605062ddc3
yj77iw0 <treeyu12345@naver.com>
2023-06-17 22:48:20 +0900

    ✨ :: Login 서버 연동을 위한 기능 구현

---
+++ lib/service/post_login_info.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mukgen_flutter_v1/model/auth/login_response.dart';
+import 'package:mukgen_flutter_v1/secret.dart';
+
+Future<LoginResponse> postLogin(String accountId, String password) async {
+
+  Map data = {
+    "accountId" : accountId,
+    "password" : password,
+  };
+
+  var body = json.encode(data);
+
+  final response = await http.post(
+      Uri.parse("$baseUrl/auth/login"),
+      headers: {"Content-Type": "application/json",},
+      body: body);
+  if (response.statusCode == 200) {
+    return LoginResponse.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+  } else{
+    print(utf8.decode(response.bodyBytes));
+    print(response.statusCode.toString());
+    throw Exception(response.body);
+  }
+}
