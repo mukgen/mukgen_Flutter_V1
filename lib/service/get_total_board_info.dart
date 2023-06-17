b816c2450b4b855d6248342d0f3fd41a7be668b5
yj77iw0 <treeyu12345@naver.com>
2023-06-17 22:50:43 +0900

    ✨ :: 토큰을 일일이 넣어주지 않아도 되도록 accessToken 사용하는 기능 추가

--- lib/service/get_total_board_info.dart
+++ lib/service/get_total_board_info.dart
@@ -1,13 +1,17 @@
 import 'dart:convert';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:mukgen_flutter_v1/model/board/total_board.dart';
 import 'package:mukgen_flutter_v1/secret.dart';
 
 Future<BoardResponse> getTotalBoardInfo() async {
+  const storage = FlutterSecureStorage();
+  dynamic accessToken = await storage.read(key: 'accessToken');
+  print(accessToken);
   final response = await http.get(
     Uri.parse("$baseUrl/board/total"),
-    headers: <String, String>{"Authorization": token},
+    headers: <String, String>{"Authorization": "Bearer $accessToken"},
   );
   if (response.statusCode == 200) {
     return BoardResponse.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
