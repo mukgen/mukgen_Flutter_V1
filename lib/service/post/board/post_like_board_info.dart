7213437876ce7faa75c1464e738795bf7aa64996
yj77iw0 <treeyu12345@naver.com>
2023-06-25 19:27:33 +0900

    ⚡️ :: 토큰 입력 방식 수정

--- lib/service/post/board/post_like_board_info.dart
+++ lib/service/post/board/post_like_board_info.dart
@@ -1,14 +1,16 @@
 import 'dart:convert';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:mukgen_flutter_v1/model/board/like_board.dart';
 import 'package:mukgen_flutter_v1/secret.dart';
 
 Future<LikeBoard> postLikeBoardInfo(int boardId) async {
-
+  const storage = FlutterSecureStorage();
+  dynamic accessToken = await storage.read(key: 'accessToken');
   final response = await http.post(
     Uri.parse("$baseUrl/like/${boardId.toString()}"),
-    headers: <String, String>{"Authorization": token},
+    headers: <String, String>{"Authorization": "Bearer $accessToken"},
   );
   if (response.statusCode == 200) {
     return LikeBoard.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
