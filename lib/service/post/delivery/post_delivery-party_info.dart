e3f40b6155787df7dfe6f1324fd550b6cfaec4ae
yj77iw0 <treeyu12345@naver.com>
2023-06-25 19:27:22 +0900

    ⚡️ :: 토큰 입력 방식 수정

--- lib/service/post/delivery/post_delivery-party_info.dart
+++ lib/service/post/delivery/post_delivery-party_info.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:mukgen_flutter_v1/secret.dart';
 
-Future<int> postDeliveryParty(
-    String menu, int participantNumber, String place, String meetTime) async {
-  Map data = {
+Future<int> postDeliveryParty(String menu, int participantNumber, String place, String meetTime) async {
+  const storage = FlutterSecureStorage();
+  dynamic accessToken = await storage.read(key: 'accessToken');
+  Map<String, dynamic> data = {
     "menu": menu,
     "participantNumber": participantNumber,
     "place": place,
@@ -16,7 +18,7 @@ Future<int> postDeliveryParty(
 
   final response = await http.post(Uri.parse("$baseUrl/delivery-party"),
       headers: <String, String>{
-        "Authorization": token,
+        "Authorization": "Bearer $accessToken",
         "Content-Type": "application/json"
       },
       body: body);
