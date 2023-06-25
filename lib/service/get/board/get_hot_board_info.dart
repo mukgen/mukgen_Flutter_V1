f7380c7120ea82f25eae000cfbd2199f61673cf8
yj77iw0 <treeyu12345@naver.com>
2023-06-25 19:29:19 +0900

    ✨ :: 게시판 인기글 API 추가

---
+++ lib/service/get/board/get_hot_board_info.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:mukgen_flutter_v1/model/board/hot_board.dart';
+import 'package:mukgen_flutter_v1/secret.dart';
+
+Future<HotBoard> getHotBoardInfo() async {
+  const storage = FlutterSecureStorage();
+  dynamic accessToken = await storage.read(key: 'accessToken');
+  print(accessToken);
+  final response = await http.get(
+    Uri.parse("$baseUrl/board/hot"),
+    headers: <String, String>{"Authorization": "Bearer $accessToken"},
+  );
+  if (response.statusCode == 200) {
+    return HotBoard.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+  } else {
+    throw Exception(response.body);
+  }
+}
