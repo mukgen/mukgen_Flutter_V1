6941870abc36eed8024d10109f88145b2b20f35c
yj77iw0 <treeyu12345@naver.com>
2023-06-25 19:41:04 +0900

    ✨ :: 먹젠픽 페이지 추가

---
+++ lib/screen/board/mukgen_pick_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MukgenPickPage extends StatefulWidget {
+  const MukgenPickPage({Key? key}) : super(key: key);
+
+  @override
+  State<MukgenPickPage> createState() => _MukgenPickPageState();
+}
+
+class _MukgenPickPageState extends State<MukgenPickPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
