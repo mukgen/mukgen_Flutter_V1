da5f33adb6ae37dd3a8cf55d5e51714666cb22d8
yj77iw0 <treeyu12345@naver.com>
2023-07-10 22:33:14 +0900

    ✨ :: 클래스 위젯으로 분할한 배달파티 리스트 조회 기능 적용

--- lib/screen/delivery/main_delivery_party_page.dart
+++ lib/screen/delivery/main_delivery_party_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 import 'package:mukgen_flutter_v1/common/common.dart';
+import 'package:mukgen_flutter_v1/model/delivery/list_delivery-party.dart';
 import 'package:mukgen_flutter_v1/screen/delivery/delivery_what_food_page.dart';
+import 'package:mukgen_flutter_v1/service/get/delivery/get_list_delivery-party_info.dart';
+import 'package:mukgen_flutter_v1/widget/delivery_party_check.dart';
 import 'package:transition/transition.dart';
 
 class MainDeliveryPartyPage extends StatefulWidget {
@@ -11,11 +15,25 @@ class MainDeliveryPartyPage extends StatefulWidget {
   State<MainDeliveryPartyPage> createState() => _MainDeliveryPartyPageState();
 }
 
-class _MainDeliveryPartyPageState extends State<MainDeliveryPartyPage> {
+class _MainDeliveryPartyPageState extends State<MainDeliveryPartyPage> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MukGenColor.white,
+      appBar: AppBar(
+        backgroundColor: MukGenColor.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          '배달파티',
+          style: TextStyle(
+            color: MukGenColor.primaryLight1,
+            fontSize: 20.sp,
+            fontFamily: 'MukGenSemiBold',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: DeliveryPartyCheck(),
       floatingActionButton: SizedBox(
         width: 70.0.w,
         height: 70.0.h,
