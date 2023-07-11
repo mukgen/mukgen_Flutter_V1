cf97ef48c96c6b141b717034287efb67cdb15b73
yj77iw0 <treeyu12345@naver.com>
2023-07-11 11:20:47 +0900

    ✨ :: 배달파티 탈퇴 API 추가

---
+++ lib/service/post/delivery/post_leave_delivery-party_info.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:mukgen_flutter_v1/secret.dart';
+
+Future<int> postJoinDeliveryParty(int deliveryId) async {
+  const storage = FlutterSecureStorage();
+  dynamic accessToken = await storage.read(key: 'accessToken');
+  final response = await http.post(Uri.parse("$baseUrl/delivery-party/leave/$deliveryId"),
+    headers: <String, String>{
+      "Authorization": "Bearer $accessToken",
+    },
+  );
+  if (response.statusCode == 200) {
+    print("성공");
+    return 1;
+  } else {
+    print("실패");
+    print(response.statusCode.toString()); // 오류 코드 출력
+    print(utf8.decode(response.bodyBytes)); // 서버에서 주는 오류 메시지 출력
+    return 0;
+  }
+}
