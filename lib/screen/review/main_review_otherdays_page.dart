0d294b1ea07cb7c93452f52a51e5ae2c8acf8047
yj77iw0 <treeyu12345@naver.com>
2023-07-27 11:03:35 +0900

    🔥 :: 불필요한 코드 삭제

--- lib/screen/review/main_review_otherdays_page.dart
+++ lib/screen/review/main_review_otherdays_page.dart
@@ -26,9 +26,7 @@ class _MainReviewOtherDaysPageState extends State<MainReviewOtherDaysPage> {
           padding: EdgeInsets.only(left: 20.0.w),
           child: GestureDetector(
             onTap: () {
-              setState(() {
-                Navigator.of(context).pop();
-              });
+              Navigator.of(context).pop();
             },
             child: Icon(
               Icons.arrow_back_ios_new,
