61dc4097ab939ae18cbe18d8b32f7191d42180b3
yj77iw0 <treeyu12345@naver.com>
2023-07-31 17:17:14 +0900

    ✨ :: 가로 방지, 텍스트 크기 고정 기능, 스플래쉬 화면 지속시간 수정

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,15 @@ import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mukgen_flutter_v1/screen/starting_page.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(MyApp());
+  });
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -13,6 +21,12 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: Size(393, 852),
       builder: (context, child) => MaterialApp(
+        builder: (context, child) {
+          return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+              child: child!,
+          );
+        },
         home: SplashPage(),
         debugShowCheckedModeBanner: false,
       ),
@@ -29,7 +43,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(milliseconds: 3000), () {
+    Future.delayed(Duration(milliseconds: 1100), () {
       Navigator.push(
         context,
         PageRouteBuilder(
