2402c867fcc9a51f2e5806dfd5562361a029d020
yj77iw0 <treeyu12345@naver.com>
2023-08-11 13:47:30 +0900

    ✨ :: 이메일, 중복확인 유효성 검사 함수 추가

--- lib/widget/text_field_validation.dart
+++ lib/widget/text_field_validation.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mukgen_flutter_v1/common/common.dart';
+import 'package:mukgen_flutter_v1/service/get/auth/get_duplicate_info.dart';
+import 'package:mukgen_flutter_v1/widget/text_field_duplicate.dart';
 
 class Validation {
-  static String getValidation(
-      String input, String inputCondition, int minLength, int maxLength, String fieldName) {
+  static String getValidation(String input, String inputCondition,
+      int minLength, int maxLength, String fieldName) {
     if (input.isEmpty) {
       return inputCondition;
     } else if (input.length < minLength || input.length > maxLength) {
@@ -13,9 +15,36 @@ class Validation {
     }
   }
 
+  static String getEmailValidation(String input) {
+    RegExp emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+$');
+    if (input.isNotEmpty) {
+      if (!emailRegex.hasMatch(input)) {
+        return "잘못된 유형의 이메일 주소입니다.";
+      }
+    }
+    return "";
+  }
+
+  static String getIdValidation(String input) {
+    if (input.isEmpty) {
+      return '최소 5자, 최대 15자';
+    } else if (input.length < 5 || input.length > 15) {
+      return "조건에 맞지 않습니다.";
+    } else {
+      if (DuplicateResult.isDuplicate == true) {
+        if (DuplicateResult.duplicateState == true) {
+          return "중복된 아이디입니다.";
+        } else if (DuplicateResult.duplicateState == false) {
+          return "사용 가능한 아이디입니다.";
+        }
+      }
+      return "중복 여부를 확인해주세요.";
+    }
+  }
+
   static String getPwdValidation(String password, {String? confirmPassword}) {
     RegExp passwordRegex =
-    RegExp(r'^(?=.*[!@#$%^&*])(?=.{1,20}$).*'); // 비밀번호 정규식
+        RegExp(r'^(?=.*[!@#$%^&*])(?=.{1,20}$).*'); // 비밀번호 정규식
 
     if (password.isEmpty) {
       return "특수문자 최소 1자 포함, 최대 20자";
@@ -23,7 +52,7 @@ class Validation {
       return "비밀번호가 일치하지 않습니다.";
     } else if (!passwordRegex.hasMatch(password)) {
       return "조건에 맞지 않습니다.";
-    } else if (confirmPassword != null && password == confirmPassword){
+    } else if (confirmPassword != null && password == confirmPassword) {
       return "비밀번호가 일치합니다.";
     } else {
       return "사용 가능한 비밀번호입니다.";
@@ -31,7 +60,8 @@ class Validation {
   }
 
   static bool _getValidationColor(String input, int minLength, int maxLength) {
-    return input.isEmpty || (input.length >= minLength && input.length <= maxLength);
+    return input.isEmpty ||
+        (input.length >= minLength && input.length <= maxLength);
   }
 
   static Color getFieldColor(String input, int minLength, int maxLength) {
@@ -44,9 +74,34 @@ class Validation {
     }
   }
 
+  static Color getIdFieldColor(String input) {
+    if (input.isEmpty) {
+      return MukGenColor.primaryLight2;
+    } else if (input.length < 5 || input.length > 15) {
+      return MukGenColor.red;
+    } else {
+      if (DuplicateResult.isDuplicate == true) {
+        if (DuplicateResult.duplicateState == true) {
+          return MukGenColor.red;
+        } else if (DuplicateResult.duplicateState == false) {
+          return MukGenColor.green;
+        }
+      }
+      return MukGenColor.red;
+    }
+  }
+
+  static Color? getEmailFieldColor(String input) {
+    RegExp emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+$');
+    if (!emailRegex.hasMatch(input)) {
+      return MukGenColor.red;
+    }
+    return null;
+  }
+
   static Color getPwdFieldColor(String password, {String? confirmPassword}) {
     RegExp passwordRegex =
-    RegExp(r'^(?=.*[!@#$%^&*])(?=.{1,20}$).*'); // 비밀번호 정규식
+        RegExp(r'^(?=.*[!@#$%^&*])(?=.{1,20}$).*'); // 비밀번호 정규식
 
     if (password.isEmpty) {
       return MukGenColor.primaryLight2;
@@ -61,5 +116,3 @@ class Validation {
     }
   }
 }
-
-
