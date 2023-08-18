e1583a26e35b11ebb15fe71c4ea5d9e5e2568a69
iyes826 <treeyu12345@naver.com>
2023-08-18 15:54:19 +0900

    ✨ :: 내가 작성한 글 커스텀 위젯 추가

---
+++ lib/widget/user/my_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mukgen_flutter_v1/common/common.dart';
+
+class MyWidget extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  const MyWidget({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+
+      },
+      child: Column(
+        children: [
+          Container(
+            height: 60.0.h,
+            width: 60.0.w,
+            decoration: BoxDecoration(
+              color: MukGenColor.primaryLight3,
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Icon(
+                icon,
+                color: MukGenColor.primaryBase,
+                size: 28.sp,
+              ),
+            ),
+          ),
+          SizedBox(height: 6.0.h),
+          Text(
+            title,
+            style: TextStyle(
+              color: MukGenColor.black,
+              fontSize: 12.sp,
+              fontFamily: 'MukgenRegular',
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
