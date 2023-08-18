0551ce1ffe2ead6e449a36740f3137dd37aae818
iyes826 <treeyu12345@naver.com>
2023-08-18 15:54:31 +0900

    ✨ :: 회원 정보 위젯 추가

---
+++ lib/widget/user/user_info_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mukgen_flutter_v1/common/common.dart';
+
+class UserInfoWidget extends StatelessWidget {
+  final String title;
+  final String content;
+  const UserInfoWidget({Key? key, required this.title, required this.content}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 34.0.w),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: MukGenColor.primaryLight1,
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'MukgenSemiBold',
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 34.0.w),
+          child: Text(
+            content,
+            style: TextStyle(
+              color: MukGenColor.primaryDark2,
+              fontSize: 16.sp,
+              fontFamily: 'MukgenRegular',
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
