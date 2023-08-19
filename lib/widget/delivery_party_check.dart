6beef2d6bc51500b7b249f580946b3140598ad10
iyes826 <treeyu12345@naver.com>
2023-08-19 19:07:37 +0900

    🚚 :: 필드 이름 변경

--- lib/widget/delivery_party_check.dart
+++ lib/widget/delivery_party_check.dart
@@ -166,7 +166,7 @@ class _DeliveryPartyCheckState extends State<DeliveryPartyCheck> with SingleTick
                                                               children: [
                                                                 SizedBox(width: 5.0.w),
                                                                 Text(
-                                                                  listDeliveryParty.deliveryPartyResponseList![index].userInfoResponseList![0].name.toString(),
+                                                                  listDeliveryParty.deliveryPartyResponseList![index].userInfoResponseList![0].nickname.toString(),
                                                                   style: TextStyle(
                                                                     color: entered ? MukGenColor.pointLight4 : MukGenColor.black,
                                                                     fontSize: 12.sp,
@@ -276,7 +276,7 @@ class _DeliveryPartyCheckState extends State<DeliveryPartyCheck> with SingleTick
                                                                   ),
                                                                 ),
                                                                 Text(
-                                                                  listDeliveryParty.deliveryPartyResponseList![index].userInfoResponseList![index1+1].name!.length > 3 ? '${listDeliveryParty.deliveryPartyResponseList![index].userInfoResponseList![index1+1].name!.substring(0, 3)}...' : listDeliveryParty.deliveryPartyResponseList![index].userInfoResponseList![index1+1].name.toString(),
+                                                                  listDeliveryParty.deliveryPartyResponseList![index].userInfoResponseList![index1+1].nickname!.length > 3 ? '${listDeliveryParty.deliveryPartyResponseList![index].userInfoResponseList![index1+1].nickname!.substring(0, 3)}...' : listDeliveryParty.deliveryPartyResponseList![index].userInfoResponseList![index1+1].nickname.toString(),
                                                                   overflow: TextOverflow.ellipsis,
                                                                   maxLines: 1,
                                                                   style: TextStyle(
