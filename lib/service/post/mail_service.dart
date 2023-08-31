780f1a5427e43457464ba69612674dea2bd6be7f
yoochanhong <chanhongy6@naver.com>
2023-08-31 13:25:10 +0900

    ✨ :: Mail service 클래스 추가

---
+++ lib/service/post/mail_service.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mukgen_flutter_v1/secret.dart';
+
+class MailService {
+  static const _url = "$baseUrl/mail";
+
+  static Future<int> postSendMailInfo(String mail) async {
+    Map<String, dynamic> data = {
+      "mail": mail,
+    };
+
+    final response = await http.post(
+      Uri.parse("$_url/send"),
+      headers: {
+        "Content-Type": "application/json",
+        "X-Not-Using-Xquare-Auth": "true",
+      },
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode != 200) {
+      return 0;
+    }
+    debugPrint(utf8.decode(response.bodyBytes));
+    return 1;
+  }
+
+  static Future<bool> postAuthenticateMailInfo(String mail, String code) async {
+    Map<String, dynamic> data = {
+      "mail": mail,
+      "code": code,
+    };
+
+    final response = await http.post(
+      Uri.parse("$_url/authenticate"),
+      headers: {
+        "Content-Type": "application/json",
+        "X-Not-Using-Xquare-Auth": "true",
+      },
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode != 200) {
+      return false;
+    }
+    debugPrint(utf8.decode(response.bodyBytes));
+    return true;
+  }
+}
