ec7b793a274d7e22f0589e143cc0a5b9e6bea67e
yoochanhong <chanhongy6@naver.com>
2023-08-31 21:04:20 +0900

    ✨ :: Meal service 클래스 추가

---
+++ lib/service/meal_service.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mukgen_flutter_v1/model/meal/mukgen_pick.dart';
+import 'package:mukgen_flutter_v1/model/meal/today_meal.dart';
+import 'package:mukgen_flutter_v1/secret.dart';
+import 'package:http/http.dart' as http;
+
+class MealService {
+  static const _storage = FlutterSecureStorage();
+  static const _url = "$baseUrl/meal";
+
+  MealService._();
+
+  Future<MukGenPickBoard> getMukGenPickInfo() async {
+    dynamic accessToken = await _storage.read(key: 'accessToken');
+    final response = await http.get(
+      Uri.parse("$baseUrl/mukgen-pick"),
+      headers: <String, String>{
+        "Authorization": "Bearer $accessToken",
+        "X-Not-Using-Xquare-Auth": "true"
+      },
+    );
+    if (response.statusCode != 200) {
+      throw Exception(response.body);
+    }
+    return MukGenPickBoard.fromJson(
+        jsonDecode(utf8.decode(response.bodyBytes)));
+  }
+
+  Future<TodayMeal> getTodayMealInfo() async {
+    dynamic accessToken = await _storage.read(key: 'accessToken');
+    final response = await http.get(
+      Uri.parse("$_url/today"),
+      headers: <String, String>{
+        "Authorization": "Bearer $accessToken",
+        "X-Not-Using-Xquare-Auth": "true"
+      },
+    );
+    if (response.statusCode != 200) {
+      throw Exception(response.body);
+    }
+    return TodayMeal.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+  }
+}
