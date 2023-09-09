7ffa8ecf1aa1c33924a3a88649a671b1efc920fb
iyes826 <treeyu12345@naver.com>
2023-09-10 01:47:25 +0900

    📝 :: 클래스명 변경

--- lib/screen/sign_in/bloc/sign_in_event.dart
+++ lib/screen/sign_in/bloc/sign_in_event.dart
@@ -2,11 +2,11 @@ import 'package:equatable/equatable.dart';
 
 abstract class SignInEvent extends Equatable {}
 
-class PressedButton extends SignInEvent {
+class LoadSignIn extends SignInEvent {
   final String accountId;
   final String password;
 
-  PressedButton({required this.accountId, required this.password});
+  LoadSignIn({required this.accountId, required this.password});
 
   @override
   List<Object?> get props => [accountId, password];
