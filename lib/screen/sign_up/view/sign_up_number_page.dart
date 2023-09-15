81b6d9552051d06f5733315abcccb134b5306dae
iyes826 <treeyu12345@naver.com>
2023-09-15 16:14:43 +0900

    ➖ :: 의존성 제거

--- lib/screen/sign_up/view/sign_up_number_page.dart
+++ lib/screen/sign_up/view/sign_up_number_page.dart
@@ -5,7 +5,6 @@ import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_bloc.dart';
 import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_event.dart';
 import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_state.dart';
 import 'package:mukgen_flutter_v1/screen/starting_page.dart';
-import 'package:mukgen_flutter_v1/service/auth_service.dart';
 import 'package:mukgen_flutter_v1/widget/mukgen_button.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mukgen_flutter_v1/widget/mukgen_text_field.dart';
