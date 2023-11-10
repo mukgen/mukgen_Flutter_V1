e8b29c39ce982d6ebe2a08c056c1fc125d4afef8
yoochanhong <chanhongy6@naver.com>
2023-11-10 23:48:14 +0900

    ✨:: BoardEndpoint updateBoard 추가

--- lib/data/data_source/board/remote/board_endpoint.dart
+++ lib/data/data_source/board/remote/board_endpoint.dart
@@ -3,6 +3,7 @@ import 'package:mukgen_flutter_v1/core/network/http_method.dart';
 import 'package:mukgen_flutter_v1/core/network/mukgen_endpoint.dart';
 import 'package:mukgen_flutter_v1/core/network/mukgen_rest_api_domain.dart';
 import 'package:mukgen_flutter_v1/data/dto/board/request/create_board_request_dto.dart';
+import 'package:mukgen_flutter_v1/data/dto/board/request/update_board_request_dto.dart';
 
 sealed class BoardEndpoint extends MukgenEndpoint {
   BoardEndpoint();
@@ -10,10 +11,16 @@ sealed class BoardEndpoint extends MukgenEndpoint {
   factory BoardEndpoint.createBoard(
       {required CreateBoardRequestDTO createBoardRequestDTO}) = CreateBoard;
 
+  factory BoardEndpoint.updateBoard(
+      {required int boardId,
+      required UpdateBoardRequestDTO updateBoardRequestDTO}) = UpdateBoard;
+
   @override
   BaseRequestDTO? get body => switch (this) {
         CreateBoard(createBoardRequestDTO: final createBoardBody) =>
           createBoardBody,
+        UpdateBoard(updateBoardRequestDTO: final updateBoardBody) =>
+          updateBoardBody,
       };
 
   @override
@@ -25,15 +32,20 @@ sealed class BoardEndpoint extends MukgenEndpoint {
   @override
   HTTPMethod get httpMethod => switch (this) {
         CreateBoard() => HTTPMethod.post,
+        UpdateBoard() => HTTPMethod.put,
       };
 
   @override
   JwtTokenType get jwtTokenType => switch (this) {
         CreateBoard() => JwtTokenType.accessToken,
+        UpdateBoard() => JwtTokenType.accessToken,
       };
 
   @override
-  String get path => switch (this) { CreateBoard() => "/board/" };
+  String get path => switch (this) {
+        CreateBoard() => "/board/",
+        UpdateBoard(boardId: final boardId) => "/board/$boardId",
+      };
 
   @override
   Map<String, dynamic>? get queryParam => null;
@@ -43,4 +55,11 @@ final class CreateBoard extends BoardEndpoint {
   final CreateBoardRequestDTO createBoardRequestDTO;
 
   CreateBoard({required this.createBoardRequestDTO});
+}
+
+final class UpdateBoard extends BoardEndpoint {
+  final int boardId;
+  final UpdateBoardRequestDTO updateBoardRequestDTO;
+
+  UpdateBoard({required this.boardId, required this.updateBoardRequestDTO});
 }
