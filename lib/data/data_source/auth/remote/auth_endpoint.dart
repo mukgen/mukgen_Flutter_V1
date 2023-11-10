d405bf76861bc74fa2b46cbb57fe77ab1a05e335
yoochanhong <chanhongy6@naver.com>
2023-11-10 20:10:57 +0900

    ✨ :: AuthEndpoint signup 추가

--- lib/data/data_source/auth/remote/auth_endpoint.dart
+++ lib/data/data_source/auth/remote/auth_endpoint.dart
@@ -4,6 +4,7 @@ import 'package:mukgen_flutter_v1/core/network/mukgen_endpoint.dart';
 import 'package:mukgen_flutter_v1/core/network/mukgen_rest_api_domain.dart';
 import 'package:mukgen_flutter_v1/data/dto/auth/request/re_issue_request_dto.dart';
 import 'package:mukgen_flutter_v1/data/dto/auth/request/sign_in_request_dto.dart';
+import 'package:mukgen_flutter_v1/data/dto/auth/request/sign_up_request_dto.dart';
 
 sealed class AuthEndpoint extends MukgenEndpoint {
   AuthEndpoint();
@@ -14,10 +15,14 @@ sealed class AuthEndpoint extends MukgenEndpoint {
   factory AuthEndpoint.reIssue({required ReIssueRequestDTO reIssueRequestDTO}) =
       ReIssue;
 
+  factory AuthEndpoint.signUp({required SignUpRequestDTO signUpRequestDTO}) =
+      SignUp;
+
   @override
   BaseRequestDTO? get body => switch (this) {
         SignIn(signInRequestDTO: final signInBody) => signInBody,
         ReIssue(reIssueRequestDTO: final reIssueBody) => reIssueBody,
+        SignUp(signUpRequestDTO: final signUpBody) => signUpBody,
       };
 
   @override
@@ -27,6 +32,7 @@ sealed class AuthEndpoint extends MukgenEndpoint {
   JwtTokenType get jwtTokenType => switch (this) {
         SignIn() => JwtTokenType.none,
         ReIssue() => JwtTokenType.accessToken,
+        SignUp() => JwtTokenType.none,
       };
 
   @override
@@ -36,24 +42,34 @@ sealed class AuthEndpoint extends MukgenEndpoint {
   HTTPMethod get httpMethod => switch (this) {
         SignIn() => HTTPMethod.post,
         ReIssue() => HTTPMethod.post,
+        SignUp() => HTTPMethod.post,
       };
 
   @override
-  String get path =>
-      switch (this) { SignIn() => "/login", ReIssue() => "/re-issue" };
+  String get path => switch (this) {
+        SignIn() => "/login",
+        ReIssue() => "/re-issue",
+        SignUp() => "/signup/general",
+      };
 
   @override
   Map<String, dynamic>? get queryParam => null;
 }
 
 final class SignIn extends AuthEndpoint {
-  SignInRequestDTO signInRequestDTO;
+  final SignInRequestDTO signInRequestDTO;
 
   SignIn({required this.signInRequestDTO});
 }
 
 final class ReIssue extends AuthEndpoint {
-  ReIssueRequestDTO reIssueRequestDTO;
+  final ReIssueRequestDTO reIssueRequestDTO;
 
   ReIssue({required this.reIssueRequestDTO});
+}
+
+final class SignUp extends AuthEndpoint {
+  final SignUpRequestDTO signUpRequestDTO;
+
+  SignUp({required this.signUpRequestDTO});
 }
