bcc7c239119a47e8033191b13e3e4de2d0b08a73
yoochanhong <chanhongy6@naver.com>
2023-11-11 17:44:36 +0900

    ✨ :: MealSuggestion updateMealSuggestion 추가

--- lib/data/data_source/meal_suggestion/remote/meal_suggestion_endpoint.dart
+++ lib/data/data_source/meal_suggestion/remote/meal_suggestion_endpoint.dart
@@ -3,6 +3,7 @@ import 'package:mukgen_flutter_v1/core/network/http_method.dart';
 import 'package:mukgen_flutter_v1/core/network/mukgen_endpoint.dart';
 import 'package:mukgen_flutter_v1/core/network/mukgen_rest_api_domain.dart';
 import 'package:mukgen_flutter_v1/data/dto/meal_suggestion/request/create_meal_suggestion_request_dto.dart';
+import 'package:mukgen_flutter_v1/data/dto/meal_suggestion/request/update_meal_suggestion_request_dto.dart';
 
 sealed class MealSuggestionEndpoint extends MukgenEndpoint {
   MealSuggestionEndpoint();
@@ -11,12 +12,21 @@ sealed class MealSuggestionEndpoint extends MukgenEndpoint {
       {required CreateMealSuggestionRequestDTO
           createMealSuggestionRequestDTO}) = CreateMealSuggestion;
 
+  factory MealSuggestionEndpoint.updateMealSuggestion(
+      {required int mealSuggestionId,
+      required UpdateMealSuggestionRequestDTO
+          updateMealSuggestionRequestDTO}) = UpdateMealSuggestion;
+
   @override
   BaseRequestDTO? get body => switch (this) {
         CreateMealSuggestion(
           createMealSuggestionRequestDTO: final createMealSuggestionBody
         ) =>
           createMealSuggestionBody,
+        UpdateMealSuggestion(
+          updateMealSuggestionRequestDTO: final updateMealSuggestionBody
+        ) =>
+          updateMealSuggestionBody,
       };
 
   @override
@@ -28,21 +38,26 @@ sealed class MealSuggestionEndpoint extends MukgenEndpoint {
   @override
   HTTPMethod get httpMethod => switch (this) {
         CreateMealSuggestion() => HTTPMethod.post,
+        UpdateMealSuggestion() => HTTPMethod.put,
       };
 
   @override
   JwtTokenType get jwtTokenType => switch (this) {
         CreateMealSuggestion() => JwtTokenType.accessToken,
+        UpdateMealSuggestion() => JwtTokenType.accessToken,
       };
 
   @override
   String get path => switch (this) {
         CreateMealSuggestion() => "/meal-suggestion",
+        UpdateMealSuggestion(mealSuggestionId: final mealSuggestionId) =>
+          "/meal-suggestion/$mealSuggestionId",
       };
 
   @override
   Map<String, dynamic>? get queryParam => switch (this) {
         CreateMealSuggestion() => null,
+        UpdateMealSuggestion() => null,
       };
 }
 
@@ -50,4 +65,14 @@ final class CreateMealSuggestion extends MealSuggestionEndpoint {
   final CreateMealSuggestionRequestDTO createMealSuggestionRequestDTO;
 
   CreateMealSuggestion({required this.createMealSuggestionRequestDTO});
+}
+
+final class UpdateMealSuggestion extends MealSuggestionEndpoint {
+  final int mealSuggestionId;
+  final UpdateMealSuggestionRequestDTO updateMealSuggestionRequestDTO;
+
+  UpdateMealSuggestion({
+    required this.mealSuggestionId,
+    required this.updateMealSuggestionRequestDTO,
+  });
 }
