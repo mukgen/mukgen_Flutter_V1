dee49388862e2922993c5966145eaea148162f7a
yoochanhong <chanhongy6@naver.com>
2023-11-13 15:17:13 +0900

    ♻️ :: Today => Day

--- lib/domain/entity/review/simple_review_for_day_entity.dart
+++ lib/domain/entity/review/simple_review_for_day_entity.dart
@@ -1,11 +1,11 @@
 import 'package:mukgen_flutter_v1/data/dto/meal/request/read_single_meal_request_dto.dart';
 
-class SimpleReviewForTodayEntity {
+class SimpleReviewForDayEntity {
   final int riceId, count, reviewId;
   final String review, userNickname, createdAt;
   final RiceType riceType;
 
-  SimpleReviewForTodayEntity({
+  SimpleReviewForDayEntity({
     required this.riceId,
     required this.count,
     required this.reviewId,
