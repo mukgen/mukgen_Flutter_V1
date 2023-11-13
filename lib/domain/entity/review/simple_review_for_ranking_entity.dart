e2cd476ae2b930959a09246bb6e44ead9949b89c
yoochanhong <chanhongy6@naver.com>
2023-11-13 11:40:09 +0900

    ✨ :: 리뷰 랭킹 표시용 entity 클래스

---
+++ lib/domain/entity/review/simple_review_for_ranking_entity.dart
@@ -0,0 +1,11 @@
+class SimpleReviewForRankingEntity {
+  final int reviewCount, averageReview;
+  final String profileUrl, userNickname;
+
+  SimpleReviewForRankingEntity({
+    required this.reviewCount,
+    required this.averageReview,
+    required this.profileUrl,
+    required this.userNickname,
+  });
+}
