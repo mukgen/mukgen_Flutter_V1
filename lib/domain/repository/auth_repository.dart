bc5909228a2852e502aad8e243f136d076a94fca
yoochanhong <chanhongy6@naver.com>
2023-11-14 21:22:48 +0900

    ✨ :: auth repository 정의

---
+++ lib/domain/repository/auth_repository.dart
@@ -0,0 +1,22 @@
+import 'package:mukgen_flutter_v1/core/network/result.dart';
+import 'package:mukgen_flutter_v1/data/dto/auth/request/change_password_request_dto.dart';
+import 'package:mukgen_flutter_v1/data/dto/auth/request/re_issue_request_dto.dart';
+import 'package:mukgen_flutter_v1/data/dto/auth/request/sign_in_request_dto.dart';
+import 'package:mukgen_flutter_v1/data/dto/auth/request/sign_up_request_dto.dart';
+import 'package:mukgen_flutter_v1/domain/entity/auth/jwt_token_entity.dart';
+
+abstract class AuthRepository {
+  Future<Result<JWTTokenEntity, Exception>> signIn(
+      {required SignInRequestDTO signInRequestDTO});
+
+  Future<Result<JWTTokenEntity, Exception>> reIssue(
+      {required ReIssueRequestDTO reIssueRequestDTO});
+
+  Future<Result<void, Exception>> signUp(
+      {required SignUpRequestDTO signUpRequestDTO});
+
+  Future<Result<void, Exception>> changePassword(
+      {required ChangePasswordRequestDTO changePasswordRequestDTO});
+
+  Future<Result<bool, Exception>> idDuplicateCheck({required String accountId});
+}
