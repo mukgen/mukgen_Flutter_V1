cf39727fffa8037eb69425f3a9b17eaba546bd6b
yoochanhong <chanhongy6@naver.com>
2023-11-16 23:26:57 +0900

    ✨ :: 아이디 중복체크 use case 추가

---
+++ lib/domain/use_case/auth/id_duplicate_check_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:mukgen_flutter_v1/core/network/result.dart';
+import 'package:mukgen_flutter_v1/domain/repository/auth_repository.dart';
+
+class IdDuplicateCheckUseCase {
+  final AuthRepository _authRepository;
+
+  IdDuplicateCheckUseCase({required AuthRepository authRepository})
+      : _authRepository = authRepository;
+
+  Future<Result<void, Exception>> execute({required String accountId}) =>
+      _authRepository.idDuplicateCheck(accountId: accountId);
+}
