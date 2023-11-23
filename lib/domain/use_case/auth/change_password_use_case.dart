544ab6b21cac5ba8b3598ef10982f1a6524ec7b4
yoochanhong <chanhongy6@naver.com>
2023-11-23 15:20:58 +0900

    ♻️ :: auth repository 폴더 구조 변경

--- lib/domain/use_case/auth/change_password_use_case.dart
+++ lib/domain/use_case/auth/change_password_use_case.dart
@@ -1,6 +1,6 @@
 import 'package:mukgen_flutter_v1/core/network/result.dart';
 import 'package:mukgen_flutter_v1/data/dto/auth/request/change_password_request_dto.dart';
-import 'package:mukgen_flutter_v1/domain/repository/auth_repository.dart';
+import 'package:mukgen_flutter_v1/domain/repository/auth_repository/auth_repository.dart';
 
 class ChangePasswordUseCase {
   final AuthRepository _authRepository;
--- lib/domain/use_case/auth/id_duplicate_check_use_case.dart
+++ lib/domain/use_case/auth/id_duplicate_check_use_case.dart
@@ -1,5 +1,5 @@
 import 'package:mukgen_flutter_v1/core/network/result.dart';
-import 'package:mukgen_flutter_v1/domain/repository/auth_repository.dart';
+import 'package:mukgen_flutter_v1/domain/repository/auth_repository/auth_repository.dart';
 
 class IdDuplicateCheckUseCase {
   final AuthRepository _authRepository;
