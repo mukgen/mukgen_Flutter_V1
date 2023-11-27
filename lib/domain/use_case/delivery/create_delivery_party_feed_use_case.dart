a179cdcf349ccd6040506568341d0c7f24702236
yoochanhong <chanhongy6@naver.com>
2023-11-27 19:22:42 +0900

    ✨ :: 배달파티 모집 글 게시하기 use case 추가

---
+++ lib/domain/use_case/delivery/create_delivery_party_feed_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:mukgen_flutter_v1/core/network/result.dart';
+import 'package:mukgen_flutter_v1/data/dto/delivery/request/create_delivery_party_feed_request_dto.dart';
+import 'package:mukgen_flutter_v1/domain/repository/delivery_repository.dart';
+
+class CreateDeliveryPartyFeedUseCase {
+  final DeliveryRepository _deliveryRepository;
+
+  CreateDeliveryPartyFeedUseCase(
+      {required DeliveryRepository deliveryRepository})
+      : _deliveryRepository = deliveryRepository;
+
+  Future<Result<void, Exception>> execute(
+          {required CreateDeliveryPartyFeedRequestDTO
+              createDeliveryPartyFeedRequestDTO}) =>
+      _deliveryRepository.createDeliveryPartyFeed(
+          createDeliveryPartyFeedRequestDTO: createDeliveryPartyFeedRequestDTO);
+}
