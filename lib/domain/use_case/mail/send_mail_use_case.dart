2a83b146d19b8a4b9c2dc8cdb16d335505a483fb
iyes826 <treeyu12345@naver.com>
2023-11-28 23:24:12 +0900

    ✨ :: 이메일 인증코드 전송 use case 추가

---
+++ lib/domain/use_case/mail/send_mail_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mukgen_flutter_v1/core/network/result.dart';
+import 'package:mukgen_flutter_v1/data/dto/auth/request/sign_up_request_dto.dart';
+import 'package:mukgen_flutter_v1/data/dto/mail/request/send_mail_auth_code_request_dto.dart';
+import 'package:mukgen_flutter_v1/domain/repository/auth_repository/auth_repository.dart';
+import 'package:mukgen_flutter_v1/domain/repository/mail_repository/mail_repository.dart';
+
+class SendMailUseCase {
+  final MailRepository _mailRepository;
+
+  SendMailUseCase({required MailRepository mailRepository})
+      : _mailRepository = mailRepository;
+
+  Future<Result<void, Exception>> execute(
+      {required SendMailAuthCodeRequestDTO sendMailAuthCodeRequestDTO}) =>
+      _mailRepository.sendMailAuthCode(sendMailAuthCodeRequestDTO: sendMailAuthCodeRequestDTO);
+}
