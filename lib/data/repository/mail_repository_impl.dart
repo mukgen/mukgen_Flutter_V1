2f30fc32ffd70cba3961dfd64d4a13eb0bc68de0
iyes826 <treeyu12345@naver.com>
2023-11-28 23:26:56 +0900

    🚚 :: mail repository 폴더 위치 이동

--- lib/data/repository/mail_repository_impl.dart
+++ lib/data/repository/mail_repository_impl.dart
@@ -2,7 +2,7 @@ import 'package:mukgen_flutter_v1/core/network/result.dart';
 import 'package:mukgen_flutter_v1/data/data_source/mail/remote/remote_mail_data_source.dart';
 import 'package:mukgen_flutter_v1/data/dto/mail/request/check_mail_auth_code_request_dto.dart';
 import 'package:mukgen_flutter_v1/data/dto/mail/request/send_mail_auth_code_request_dto.dart';
-import 'package:mukgen_flutter_v1/domain/repository/mail_repository.dart';
+import 'package:mukgen_flutter_v1/domain/repository/mail_repository/mail_repository.dart';
 
 class MailRepositoryImpl extends MailRepository {
   final RemoteMailDataSource _remoteMailDataSource;
