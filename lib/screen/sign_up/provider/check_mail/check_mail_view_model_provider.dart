5538eb290f8fef0153e00efbd7f9fc9f236aa7ce
iyes826 <treeyu12345@naver.com>
2023-11-29 22:22:12 +0900

    ✨ :: 인증코드 확인 view model provider 추가

---
+++ lib/screen/sign_up/provider/check_mail/check_mail_view_model_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mukgen_flutter_v1/domain/use_case/mail/providers/check_mail_use_case_provider.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/check_mail/state/check_mail_state.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/check_mail/state/check_mail_view_model.dart';
+
+final checkMailViewModelProvider =
+    StateNotifierProvider<CheckMailViewModel, CheckMailState>((ref) =>
+        CheckMailViewModel(checkMailUseCase: ref.watch(checkMainUseCaseProvider)));
+
+final checkMailButtonStateProvider = StateProvider<bool>((ref) => false);
