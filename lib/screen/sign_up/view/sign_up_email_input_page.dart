b7b5241802a62fd4c8d1bc4e08361854ebb987cb
iyes826 <treeyu12345@naver.com>
2023-11-29 19:43:13 +0900

    ♻️ :: 이메일 인증코드 입력 페이지 코드 리팩토링 (상태관리 적용)

--- lib/screen/sign_up/view/sign_up_email_input_page.dart
+++ lib/screen/sign_up/view/sign_up_email_input_page.dart
@@ -1,3 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mukgen_flutter_v1/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:mukgen_flutter_v1/core/constant/mukgen_color.dart';
+import 'package:mukgen_flutter_v1/data/dto/mail/request/send_mail_auth_code_request_dto.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/send_mail/send_mail_view_model_provider.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/send_mail/state/send_mail_state.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/view/sign_up_email_confirm_page.dart';
+import 'package:mukgen_flutter_v1/screen/widget/mukgen_button.dart';
+import 'package:mukgen_flutter_v1/screen/widget/mukgen_text_field.dart';
+
+class SignUpEmailInputPage extends ConsumerStatefulWidget {
+  const SignUpEmailInputPage({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<SignUpEmailInputPage> createState() =>
+      _SignUpEmailInputPageState();
+}
+
+class _SignUpEmailInputPageState extends ConsumerState<SignUpEmailInputPage> {
+  late TextEditingController emailController;
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+
+    emailController.addListener(_updateButtonState);
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
+  void _updateButtonState() {
+    ref.read(sendMailButtonStateProvider.notifier).state =
+        emailController.text.isNotEmpty;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ref.listen(sendMailViewModelProvider.select((value) => value),
+        (previous, next) {
+      switch (next) {
+        case SendMailState.loading:
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => SignUpEmailConfirmPage()));
+        default:
+          null;
+      }
+    });
+    return Scaffold(
+      backgroundColor: MukGenColor.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(59.h),
+        child: AppBar(
+          backgroundColor: MukGenColor.white,
+          elevation: 0,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back_outlined,
+              color: MukGenColor.primaryLight1,
+              size: 24.sp,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 40.h),
+          Padding(
+            padding: EdgeInsets.only(left: 20.w),
+            child: PtdTextWidget.title(
+              '이메일 인증을 진행해주세요.',
+              MukGenColor.black,
+            ),
+          ),
+          SizedBox(height: 12.h),
+          Padding(
+            padding: EdgeInsets.only(left: 20.w),
+            child: PtdTextWidget.bodyLarge(
+              '입력한 이메일로 인증번호가 전송됩니다.',
+              MukGenColor.black,
+            ),
+          ),
+          SizedBox(height: 24.h),
+          Center(
+            child: MukGenTextField(
+              width: 352,
+              height: 56,
+              controller: emailController,
+              hintText: '이메일 주소',
+              suffixText: Padding(
+                padding: EdgeInsets.only(right: 10.w),
+                child: Text(
+                  '@dsm.hs.kr',
+                  style: TextStyle(
+                    color: MukGenColor.primaryBase,
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              fontSize: 20,
+              isPwdTextField: false,
+              maxLength: null,
+              autofocus: true,
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: EdgeInsets.only(bottom: 34.h),
+            child: Center(
+              child: MukGenButton(
+                onPressed: () {
+                  ref.read(sendMailViewModelProvider.notifier).sendMail(
+                      sendMailAuthCodeRequestDTO: SendMailAuthCodeRequestDTO(
+                          mail: '${emailController.text}@dsm.hs.kr'));
+                },
+                backgroundColor: ref.watch(sendMailButtonStateProvider)
+                    ? MukGenColor.primaryBase
+                    : MukGenColor.primaryLight2,
+                width: 352,
+                height: 55,
+                text: PtdTextWidget.bodyLarge2(
+                  '다음',
+                  MukGenColor.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 // import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:flutter_screenutil/flutter_screenutil.dart';
