00e49fc2766d4ae6ea3aba42b45bdab2c1df1112
iyes826 <treeyu12345@naver.com>
2023-11-29 19:41:44 +0900

    ✨ :: 이메일 인증코드 전송 viewmodel 추가

---
+++ lib/screen/sign_up/provider/send_mail/state/send_mail_view_model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mukgen_flutter_v1/core/network/result.dart';
+import 'package:mukgen_flutter_v1/data/dto/mail/request/send_mail_auth_code_request_dto.dart';
+import 'package:mukgen_flutter_v1/domain/use_case/mail/send_mail_use_case.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/send_mail/state/send_mail_state.dart';
+
+class SendMailViewModel extends StateNotifier<SendMailState> {
+  final SendMailUseCase _sendMailUseCase;
+
+  SendMailViewModel({required SendMailUseCase sendMailUseCase})
+      : _sendMailUseCase = sendMailUseCase,
+        super(SendMailState.initial);
+
+  Future<void> sendMail({required SendMailAuthCodeRequestDTO sendMailAuthCodeRequestDTO}) async {
+    state = SendMailState.loading;
+    final res =
+    await _sendMailUseCase.execute(sendMailAuthCodeRequestDTO: sendMailAuthCodeRequestDTO);
+    switch (res) {
+      case Success():
+        state = SendMailState.success;
+      case Failure():
+        state = SendMailState.failure;
+    }
+  }
+}
