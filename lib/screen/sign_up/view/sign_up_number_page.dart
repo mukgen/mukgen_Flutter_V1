4c3de9846a3b1373494a3747b7a9c51f21bf2af5
iyes826 <treeyu12345@naver.com>
2023-12-01 23:06:22 +0900

    ♻️ :: 회원가입 번호 입력 페이지 상태관리

--- lib/screen/sign_up/view/sign_up_number_page.dart
+++ lib/screen/sign_up/view/sign_up_number_page.dart
@@ -1,220 +1,220 @@
-//   import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:mukgen_flutter_v1/common/mukgen_color.dart';
-// import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_bloc.dart';
-// import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_event.dart';
-// import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_state.dart';
-// import 'package:mukgen_flutter_v1/screen/starting_page.dart';
-// import 'package:mukgen_flutter_v1/widget/mukgen_button.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:mukgen_flutter_v1/widget/mukgen_text_field.dart';
-//
-// class SignupNumberPage extends StatefulWidget {
-//   const SignupNumberPage(
-//       {Key? key,
-//       required this.email,
-//       required this.name,
-//       required this.id,
-//       required this.pwd,
-//       required this.pwdcheck})
-//       : super(key: key);
-//   final String email, name, id, pwd, pwdcheck;
-//
-//   @override
-//   State<SignupNumberPage> createState() => _SignupNumberPageState();
-// }
-//
-// class _SignupNumberPageState extends State<SignupNumberPage> {
-//   late FocusNode _firstFocusNode;
-//   late FocusNode _secondFocusNode;
-//   late FocusNode _thirdFocusNode;
-//
-//   late TextEditingController _firstController;
-//   late TextEditingController _secondController;
-//   late TextEditingController _thirdController;
-//
-//   bool _isButtonEnabled = false;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _firstFocusNode = FocusNode();
-//     _secondFocusNode = FocusNode();
-//     _thirdFocusNode = FocusNode();
-//     _firstController = TextEditingController();
-//     _firstController.addListener(_updateButtonState);
-//     _secondController = TextEditingController();
-//     _secondController.addListener(_updateButtonState);
-//     _thirdController = TextEditingController();
-//     _thirdController.addListener(_updateButtonState);
-//   }
-//
-//   @override
-//   void dispose() {
-//     super.dispose();
-//     _firstFocusNode.dispose();
-//     _secondFocusNode.dispose();
-//     _thirdFocusNode.dispose();
-//     _firstController.dispose();
-//     _secondController.dispose();
-//     _thirdController.dispose();
-//   }
-//
-//   void _updateButtonState() {
-//     setState(() {
-//       _isButtonEnabled = _firstController.text.length == 3 &&
-//           _secondController.text.length == 4 &&
-//           _thirdController.text.length == 4;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: MukGenColor.white,
-//       appBar: AppBar(
-//         backgroundColor: MukGenColor.white,
-//         elevation: 0,
-//         leading: Padding(
-//           padding: EdgeInsets.only(left: 20.0.w),
-//           child: GestureDetector(
-//             onTap: () {
-//               Navigator.of(context).pop();
-//             },
-//             child: Icon(
-//               Icons.arrow_back,
-//               color: MukGenColor.primaryLight1,
-//               size: 24.0.sp,
-//             ),
-//           ),
-//         ),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.start,
-//         children: [
-//           SizedBox(height: 40.0.h),
-//           Container(
-//             alignment: Alignment.topLeft,
-//             margin: EdgeInsets.only(left: 20.0.w),
-//             child: Text(
-//               '전화번호를 입력해주세요.',
-//               style: TextStyle(
-//                 fontSize: 24.0.sp,
-//                 fontWeight: FontWeight.w600,
-//                 fontFamily: 'MukgenSemiBold',
-//               ),
-//             ),
-//           ),
-//           SizedBox(height: 12.0.h),
-//           Container(
-//             alignment: Alignment.topLeft,
-//             margin: EdgeInsets.only(left: 20.0.w),
-//             child: Text(
-//               '배달 파티 모집 시 사용됩니다.',
-//               style: TextStyle(
-//                   fontSize: 14.0.sp,
-//                   fontWeight: FontWeight.w400,
-//                   fontFamily: 'MukgenRegular'),
-//             ),
-//           ),
-//           SizedBox(height: 24.0.h),
-//           Row(
-//             children: [
-//               SizedBox(width: 20.0.w),
-//               MukGenTextField(
-//                 width: 98,
-//                 height: 56,
-//                 autofocus: true,
-//                 controller: _firstController,
-//                 fontSize: 20,
-//                 textInputType: TextInputType.number,
-//                 textAlign: TextAlign.center,
-//                 isPwdTextField: false,
-//                 focusNode: _firstFocusNode,
-//                 nextFocusNode: _secondFocusNode,
-//                 maxLength: 3,
-//               ),
-//               SizedBox(width: 10.0.w),
-//               Text(
-//                 '-',
-//                 style: TextStyle(
-//                   fontSize: 20.0.sp,
-//                   fontWeight: FontWeight.w600,
-//                   fontFamily: 'MukgenSemiBold',
-//                 ),
-//               ),
-//               SizedBox(width: 10.0.w),
-//               MukGenTextField(
-//                 width: 98,
-//                 height: 56,
-//                 controller: _secondController,
-//                 autofocus: false,
-//                 fontSize: 20,
-//                 textInputType: TextInputType.number,
-//                 textAlign: TextAlign.center,
-//                 isPwdTextField: false,
-//                 focusNode: _secondFocusNode,
-//                 nextFocusNode: _thirdFocusNode,
-//                 maxLength: 4,
-//               ),
-//               SizedBox(width: 10.0.w),
-//               Text(
-//                 '-',
-//                 style: TextStyle(
-//                   fontSize: 20.0.sp,
-//                   fontWeight: FontWeight.w600,
-//                   fontFamily: 'MukgenSemiBold',
-//                 ),
-//               ),
-//               SizedBox(width: 10.0.w),
-//               MukGenTextField(
-//                 width: 98,
-//                 height: 56,
-//                 controller: _thirdController,
-//                 fontSize: 20,
-//                 textAlign: TextAlign.center,
-//                 textInputType: TextInputType.number,
-//                 isPwdTextField: false,
-//                 autofocus: false,
-//                 focusNode: _thirdFocusNode,
-//                 maxLength: 4,
-//               ),
-//             ],
-//           ),
-//           const Spacer(),
-//           BlocConsumer<SignUpBloc, SignUpState>(
-//             listener: (context, state) {
-//               if (state is Loaded) {
-//                 Navigator.of(context).pushAndRemoveUntil(
-//                     MaterialPageRoute(
-//                         builder: (context) => const StartingPage()),
-//                         (route) => false);
-//               }
-//             },
-//             builder: (context, state) {
-//               return MukGenButton(
-//                 text: "완료",
-//                 width: 352,
-//                 height: 55,
-//                 backgroundColor: _isButtonEnabled
-//                     ? MukGenColor.pointLight1
-//                     : MukGenColor.primaryLight2,
-//                 fontSize: 16,
-//                 textColor: MukGenColor.white,
-//                 onPressed: () {
-//                   if (_isButtonEnabled) {
-//                     String phone = "";
-//                     phone += _firstController.text +
-//                         _secondController.text +
-//                         _thirdController.text;
-//                     context.read<SignUpBloc>().add(LoadSignUp(nickname: widget.name, accountId: widget.id, password: widget.pwd, passwordCheck: widget.pwdcheck, phoneNumber: phone, mail: widget.email));
-//                   }
-//                 },
-//               );
-//             }
-//           ),
-//         ]
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mukgen_flutter_v1/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:mukgen_flutter_v1/core/constant/mukgen_color.dart';
+import 'package:mukgen_flutter_v1/data/dto/auth/request/sign_up_request_dto.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/controller_provider.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/sign_up_view_model_provider.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/state/sign_up_state.dart';
+import 'package:mukgen_flutter_v1/screen/starting_page.dart';
+import 'package:mukgen_flutter_v1/screen/widget/mukgen_button.dart';
+import 'package:mukgen_flutter_v1/screen/widget/mukgen_text_field.dart';
+
+class SignUpNumberPage extends ConsumerStatefulWidget {
+  final String email;
+  final String id;
+  final String password;
+  final String passwordCheck;
+  final String name;
+
+  const SignUpNumberPage({
+    Key? key,
+    required this.email,
+    required this.id,
+    required this.password,
+    required this.passwordCheck,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  ConsumerState<SignUpNumberPage> createState() => _SignUpNumberPageState();
+}
+
+class _SignUpNumberPageState extends ConsumerState<SignUpNumberPage> {
+  late TextEditingController _firstController;
+  late TextEditingController _secondController;
+  late TextEditingController _thirdController;
+
+  late FocusNode _firstFocusNode;
+  late FocusNode _secondFocusNode;
+  late FocusNode _thirdFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _firstFocusNode = FocusNode();
+    _secondFocusNode = FocusNode();
+    _thirdFocusNode = FocusNode();
+    _firstController = TextEditingController();
+    _secondController = TextEditingController();
+    _thirdController = TextEditingController();
+    _firstController.addListener(_updateButtonState);
+    _secondController.addListener(_updateButtonState);
+    _thirdController.addListener(_updateButtonState);
+  }
+
+  @override
+  void dispose() {
+    _firstController.dispose();
+    _secondController.dispose();
+    _thirdController.dispose();
+    _firstFocusNode.dispose();
+    _secondFocusNode.dispose();
+    _thirdFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _updateButtonState() {
+    ref.read(controllerProvider.notifier).state =
+        _firstController.text.isNotEmpty &&
+            _secondController.text.isNotEmpty &&
+            _thirdController.text.isNotEmpty;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ref.listen(signUpViewModelProvider.select((value) => value),
+        (previous, next) {
+      switch (next) {
+        case SignUpState.success:
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const StartingPage()),
+            (route) => false,
+          );
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text('회원가입이 완료되었습니다.')));
+        case SignUpState.failure:
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text('회원가입에 실패했습니다.')));
+        default :
+          null;
+      }
+    });
+    return Scaffold(
+      backgroundColor: MukGenColor.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(59.h),
+        child: AppBar(
+          backgroundColor: MukGenColor.white,
+          elevation: 0,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back_outlined,
+              color: MukGenColor.primaryLight1,
+              size: 24.sp,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40.h),
+            PtdTextWidget.title(
+              "전화번호를 입력해주세요.",
+              MukGenColor.black,
+            ),
+            SizedBox(height: 12.h),
+            PtdTextWidget.bodyLarge(
+              "배달 파티 모집 시 사용됩니다.",
+              MukGenColor.primaryDark1,
+            ),
+            SizedBox(height: 24.h),
+            Row(
+              children: [
+                MukGenTextField(
+                  width: 98,
+                  height: 56,
+                  autofocus: true,
+                  controller: _firstController,
+                  fontSize: 20,
+                  textInputType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  isPwdTextField: false,
+                  focusNode: _firstFocusNode,
+                  nextFocusNode: _secondFocusNode,
+                  maxLength: 3,
+                ),
+                SizedBox(width: 10.0.w),
+                PtdTextWidget.subtitle(
+                  '-',
+                  MukGenColor.black,
+                ),
+                SizedBox(width: 10.0.w),
+                MukGenTextField(
+                  width: 98,
+                  height: 56,
+                  controller: _secondController,
+                  autofocus: false,
+                  fontSize: 20,
+                  textInputType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  isPwdTextField: false,
+                  focusNode: _secondFocusNode,
+                  nextFocusNode: _thirdFocusNode,
+                  maxLength: 4,
+                ),
+                SizedBox(width: 10.0.w),
+                PtdTextWidget.subtitle(
+                  '-',
+                  MukGenColor.black,
+                ),
+                SizedBox(width: 10.0.w),
+                MukGenTextField(
+                  width: 98,
+                  height: 56,
+                  controller: _thirdController,
+                  fontSize: 20,
+                  textAlign: TextAlign.center,
+                  textInputType: TextInputType.number,
+                  isPwdTextField: false,
+                  autofocus: false,
+                  focusNode: _thirdFocusNode,
+                  maxLength: 4,
+                ),
+              ],
+            ),
+            const Spacer(),
+            Padding(
+              padding: EdgeInsets.only(bottom: 34.h),
+              child: MukGenButton(
+                onPressed: () {
+                  String phoneNumber = _firstController.text +
+                      _secondController.text +
+                      _thirdController.text;
+                  ref.watch(controllerProvider)
+                      ? ref.read(signUpViewModelProvider.notifier).signUp(
+                              signUpRequestDTO: SignUpRequestDTO(
+                            accountId: widget.id,
+                            password: widget.password,
+                            mail: widget.email,
+                            nickname: widget.name,
+                            passwordCheck: widget.passwordCheck,
+                            phoneNumber: phoneNumber,
+                          ))
+                      : null;
+                },
+                text: PtdTextWidget.bodyLarge2(
+                  '완료',
+                  MukGenColor.white,
+                ),
+                width: 352,
+                height: 55,
+                backgroundColor: ref.watch(controllerProvider)
+                    ? MukGenColor.pointBase
+                    : MukGenColor.primaryLight2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
