e60db12d24b6a3dd87d4e4375ece9079c1a180e0
iyes826 <treeyu12345@naver.com>
2023-12-01 23:05:09 +0900

    ♻️ :: 회원가입 아이디 & 비밀번호 입력 페이지 상태관리

--- lib/screen/sign_up/view/sign_up_id_pw_page.dart
+++ lib/screen/sign_up/view/sign_up_id_pw_page.dart
@@ -1,281 +1,222 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mukgen_flutter_v1/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:mukgen_flutter_v1/core/constant/mukgen_color.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/controller_provider.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/id_duplicate/id_duplicate_view_model_provider.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/provider/id_duplicate/state/id_duplicate_state.dart';
+import 'package:mukgen_flutter_v1/screen/sign_up/view/sign_up_number_page.dart';
+import 'package:mukgen_flutter_v1/screen/widget/mukgen_button.dart';
+import 'package:mukgen_flutter_v1/screen/widget/mukgen_text_field.dart';
 
-class SignUpIdPwPage extends StatefulWidget {
+class SignUpIdPwPage extends ConsumerStatefulWidget {
   final String email;
   final String name;
-  const SignUpIdPwPage({Key? key, required this.email, required this.name}) : super(key: key);
+
+  const SignUpIdPwPage({Key? key, required this.email, required this.name})
+      : super(key: key);
 
   @override
-  State<SignUpIdPwPage> createState() => _SignUpIdPwPageState();
+  ConsumerState<SignUpIdPwPage> createState() => _SignUpIdPwPageState();
 }
 
-class _SignUpIdPwPageState extends State<SignUpIdPwPage> {
+class _SignUpIdPwPageState extends ConsumerState<SignUpIdPwPage> {
+  late TextEditingController _idController;
+  late TextEditingController _pwdController;
+  late TextEditingController _pwdCheckController;
+
+  RegExp regExp = RegExp(r'^(?=.*[!@#$%^&*])(?=.{1,20}$).*');
+
+  IdDuplicateState idDuplicateState = IdDuplicateState.initial;
+
+  @override
+  void initState() {
+    super.initState();
+    _idController = TextEditingController();
+    _idController.addListener(_updateIdField);
+    _pwdController = TextEditingController();
+    _pwdController.addListener(_updatePwdField);
+    _pwdCheckController = TextEditingController();
+    _pwdCheckController.addListener(_updatePwdField);
+  }
+
+  @override
+  void dispose() {
+    _idController.dispose();
+    _pwdController.dispose();
+    _pwdCheckController.dispose();
+    super.dispose();
+  }
+
+  void _updatePwdField() {
+    ref.watch(controllerProvider.notifier).state = _pwdController.text.isNotEmpty && _pwdCheckController.text.isNotEmpty;
+  }
+
+  void _updateIdField() {
+    ref.watch(idControllerProvider.notifier).state = _idController.text.isNotEmpty;
+    ref.watch(controllerProvider.notifier).state =
+        _idController.text.isNotEmpty && _pwdController.text.isNotEmpty && _pwdCheckController.text.isNotEmpty;
+    ref.invalidate(
+        idDuplicateViewModelProvider); // idController의 값이 바뀔때마다 provider 초기화
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    ref.listen(idDuplicateViewModelProvider.select((value) => value),
+        (previous, next) {
+      switch (next) {
+        case IdDuplicateState.success:
+          idDuplicateState = IdDuplicateState.success;
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text('사용 가능한 아이디입니다.')));
+        case IdDuplicateState.failure:
+          idDuplicateState = IdDuplicateState.failure;
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text('중복된 아이디입니다.')));
+        default:
+          idDuplicateState = IdDuplicateState.initial;
+      }
+    });
+    return Scaffold(
+      backgroundColor: MukGenColor.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(59.h),
+        child: AppBar(
+          backgroundColor: MukGenColor.white,
+          elevation: 0,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back_outlined,
+              color: MukGenColor.primaryLight1,
+              size: 24.sp,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40.h),
+            PtdTextWidget.title(
+              "아이디와\n비밀번호를 입력해주세요.",
+              MukGenColor.black,
+            ),
+            SizedBox(height: 24.h),
+            MukGenTextField(
+              width: 352,
+              controller: _idController,
+              hintText: "아이디",
+              helperText: "최소 5자 최대 15자",
+              color: MukGenColor.primaryLight2,
+              suffix: GestureDetector(
+                onTap: () {
+                  ref.watch(idControllerProvider)
+                      ? ref
+                          .read(idDuplicateViewModelProvider.notifier)
+                          .idDuplicate(accountId: _idController.text)
+                      : null;
+                },
+                child: Container(
+                  width: 84.w,
+                  height: 37.h,
+                  decoration: BoxDecoration(
+                    color: MukGenColor.white,
+                    borderRadius: BorderRadius.circular(6.r),
+                    border: Border.all(
+                      color: MukGenColor.primaryLight2,
+                      width: 1.w,
+                    ),
+                  ),
+                  child: Center(
+                    child: PtdTextWidget.bodyStrong2(
+                      "중복 확인",
+                      MukGenColor.primaryDark2,
+                    ),
+                  ),
+                ),
+              ),
+              fontSize: 20,
+              isPwdTextField: false,
+              maxLength: 15,
+              autofocus: true,
+            ),
+            SizedBox(height: 24.h),
+            MukGenTextField(
+              width: 352,
+              controller: _pwdController,
+              fontSize: 20,
+              hintText: "비밀번호",
+              helperText: "특수문자 최소 1자 포함, 최대 20자",
+              color: MukGenColor.primaryLight2,
+              isPwdTextField: true,
+              maxLength: 20,
+              autofocus: false,
+            ),
+            SizedBox(height: 24.h),
+            MukGenTextField(
+              width: 352,
+              controller: _pwdCheckController,
+              fontSize: 20,
+              hintText: "비밀번호 확인",
+              isPwdTextField: true,
+              maxLength: 20,
+              autofocus: false,
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: Padding(
+        padding: EdgeInsets.only(bottom: 34.h),
+        child: MukGenButton(
+          onPressed: () {
+            if (ref.watch(controllerProvider)) {
+              if (idDuplicateState == IdDuplicateState.initial) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('중복 여부를 확인해주세요.')));
+              } else if (idDuplicateState == IdDuplicateState.success) {
+                if (!regExp.hasMatch(_pwdController.text)) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('비밀번호가 조건에 맞지 않습니다.')));
+                } else {
+                  if (_pwdController.text != _pwdCheckController.text) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('비밀번호가 일치하지 않습니다.')));
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignUpNumberPage(
+                          email: widget.email,
+                          id: _idController.text,
+                          password: _pwdController.text,
+                          passwordCheck: _pwdCheckController.text,
+                          name: widget.name,
+                        ),
+                      ),
+                    );
+                  }
+                }
+              } else if (idDuplicateState == IdDuplicateState.failure) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text('중복된 아이디입니다.')));
+              }
+            }
+          },
+          text: PtdTextWidget.bodyLarge2(
+            "다음",
+            MukGenColor.white,
+          ),
+          width: 352,
+          height: 55,
+          backgroundColor: ref.watch(controllerProvider) ? MukGenColor.primaryBase : MukGenColor.primaryLight2,
+        ),
+      ),
+    );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:mukgen_flutter_v1/common/mukgen_color.dart';
-// import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_bloc.dart';
-// import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_event.dart';
-// import 'package:mukgen_flutter_v1/screen/sign_up/bloc/sign_up_state.dart';
-// import 'package:mukgen_flutter_v1/screen/sign_up/view/sign_up_number_page.dart';
-// import 'package:mukgen_flutter_v1/service/auth_service.dart';
-// import 'package:mukgen_flutter_v1/widget/mukgen_button.dart';
-// import 'package:mukgen_flutter_v1/widget/mukgen_text_field.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:mukgen_flutter_v1/widget/text_field_duplicate.dart';
-// import 'package:mukgen_flutter_v1/widget/text_field_validation.dart';
-//
-// class SignupIdPwPage extends StatefulWidget {
-//   const SignupIdPwPage({Key? key, required this.email, required this.name})
-//       : super(key: key);
-//
-//   final String email;
-//   final String name;
-//
-//   @override
-//   State<SignupIdPwPage> createState() => _SignupIdPwPageState();
-// }
-//
-// class _SignupIdPwPageState extends State<SignupIdPwPage> {
-//   late TextEditingController idController;
-//   late TextEditingController pwdController;
-//   late TextEditingController pwdCheckController;
-//
-//   bool _isButtonEnabled = false;
-//
-//   String idValid = '최소 5자, 최대 15자';
-//
-//   Color? idColor = MukGenColor.primaryLight2;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     idController = TextEditingController();
-//     idController.addListener(_updateState);
-//     pwdController = TextEditingController();
-//     pwdController.addListener(_updateState);
-//     pwdCheckController = TextEditingController();
-//     pwdCheckController.addListener(_updateState);
-//   }
-//
-//   String _previousIdValue = "";
-//
-//   void _updateState() {
-//     final currentIdValue = idController.text;
-//
-//     if (currentIdValue != _previousIdValue) {
-//       setState(() {
-//         DuplicateResult.isDuplicate = false;
-//         idValid = Validation.getIdValidation(currentIdValue);
-//         idColor = Validation.getIdFieldColor(currentIdValue);
-//         context.read<SignUpBloc>().add(ResetEvent());
-//       });
-//
-//       _previousIdValue = currentIdValue;
-//     }
-//     setState(() {
-//       _isButtonEnabled = idValid.contains("사용 가능한") &&
-//           Validation.getPwdValidation(pwdController.text).contains("사용 가능한") &&
-//           Validation.getPwdValidation(pwdCheckController.text,
-//                   confirmPassword: pwdController.text)
-//               .contains("일치합니다");
-//     });
-//   }
-//
-//   @override
-//   void dispose() {
-//     super.dispose();
-//     idController.dispose();
-//     pwdController.dispose();
-//     pwdCheckController.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: MukGenColor.white,
-//       appBar: AppBar(
-//         backgroundColor: MukGenColor.white,
-//         elevation: 0,
-//         leading: Padding(
-//           padding: EdgeInsets.only(left: 20.0.w),
-//           child: GestureDetector(
-//             onTap: () {
-//               Navigator.of(context).pop();
-//             },
-//             child: Icon(
-//               Icons.arrow_back,
-//               color: MukGenColor.primaryLight1,
-//               size: 24.0.sp,
-//             ),
-//           ),
-//         ),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.start,
-//         children: [
-//           SizedBox(height: 40.0.h),
-//           Container(
-//             alignment: Alignment.topLeft,
-//             margin: EdgeInsets.only(left: 20.0.w),
-//             child: Text(
-//               '아이디와\n비밀번호를 입력해주세요.',
-//               style: TextStyle(
-//                 fontSize: 24.0.sp,
-//                 fontWeight: FontWeight.w600,
-//                 fontFamily: 'MukgenSemiBold',
-//               ),
-//             ),
-//           ),
-//           SizedBox(height: 24.0.h),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               BlocConsumer<SignUpBloc, SignUpState>(
-//                 listener: (context, state) {
-//                   if (state is Loaded) {
-//                     idValid = '사용 가능한 아이디입니다.';
-//                     idColor = MukGenColor.green;
-//                   }
-//                 },
-//                 builder: (context, state) {
-//                   return Stack(
-//                     alignment: Alignment.topRight,
-//                     children: [
-//                       SizedBox(
-//                         width: 352.0.w,
-//                         child: TextFormField(
-//                           controller: idController,
-//                           cursorColor: MukGenColor.black,
-//                           style: TextStyle(
-//                             color: MukGenColor.black,
-//                             fontSize: 20.sp,
-//                             fontWeight: FontWeight.w600,
-//                             fontFamily: 'MukgenSemiBold',
-//                           ),
-//                           decoration: InputDecoration(
-//                             helperText: idValid,
-//                             helperStyle: TextStyle(
-//                               color: idColor,
-//                               fontSize: 16.sp,
-//                               fontFamily: 'MukgenRegular',
-//                               fontWeight: FontWeight.w400,
-//                             ),
-//                             hintText: "아이디",
-//                             hintStyle: TextStyle(
-//                               color: MukGenColor.primaryLight2,
-//                               fontSize: 20.sp,
-//                               fontWeight: FontWeight.w600,
-//                               fontFamily: 'MukgenSemiBold',
-//                             ),
-//                             enabledBorder: idController.text.isEmpty
-//                                 ? UnderlineInputBorder(
-//                                     borderSide: BorderSide(
-//                                       color: MukGenColor.primaryLight2,
-//                                       width: 2,
-//                                     ),
-//                                   )
-//                                 : UnderlineInputBorder(
-//                                     borderSide: BorderSide(
-//                                       color: MukGenColor.black,
-//                                       width: 2,
-//                                     ),
-//                                   ),
-//                             focusedBorder: UnderlineInputBorder(
-//                               borderSide: BorderSide(
-//                                 color: MukGenColor.pointBase,
-//                                 width: 2,
-//                               ),
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                       GestureDetector(
-//                         onTap: () {
-//                           context.read<SignUpBloc>().add(LoadIdDuplicate(id: idController.text));
-//                         },
-//                         child: Container(
-//                           width: 84.0.w,
-//                           height: 37.0.h,
-//                           decoration: BoxDecoration(
-//                             borderRadius: BorderRadius.circular(6.r),
-//                             border: Border.all(
-//                               color: MukGenColor.primaryLight2,
-//                             ),
-//                           ),
-//                           child: Center(
-//                             child: Text(
-//                               '중복 확인',
-//                               style: TextStyle(
-//                                 color: MukGenColor.primaryDark2,
-//                                 fontWeight: FontWeight.w600,
-//                                 fontSize: 14.sp,
-//                                 fontFamily: 'MukgenSemiBold',
-//                               ),
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   );
-//                 },
-//               ),
-//             ],
-//           ),
-//           SizedBox(height: 24.0.h),
-//           MukGenTextField(
-//             width: 352,
-//             controller: pwdController,
-//             fontSize: 20,
-//             isPwdTextField: true,
-//             autofocus: false,
-//             maxLength: 20,
-//             hintText: "비밀번호",
-//             color: Validation.getPwdFieldColor(pwdController.text),
-//             helperText: Validation.getPwdValidation(pwdController.text),
-//           ),
-//           SizedBox(height: 24.0.h),
-//           MukGenTextField(
-//             width: 352,
-//             controller: pwdCheckController,
-//             fontSize: 20,
-//             isPwdTextField: true,
-//             autofocus: false,
-//             maxLength: 20,
-//             hintText: "비밀번호 확인",
-//             color: Validation.getPwdFieldColor(pwdCheckController.text,
-//                 confirmPassword: pwdController.text),
-//             helperText: Validation.getPwdValidation(pwdCheckController.text,
-//                 confirmPassword: pwdController.text),
-//           ),
-//           const Spacer(),
-//           MukGenButton(
-//             width: 352,
-//             height: 55,
-//             text: "다음",
-//             backgroundColor:
-//                 _isButtonEnabled ? MukGenColor.grey : MukGenColor.primaryLight2,
-//             textColor: MukGenColor.white,
-//             fontSize: 16,
-//             onPressed: () {
-//               _isButtonEnabled
-//                   ? Navigator.of(context).push(MaterialPageRoute(
-//                       builder: (context) => SignupNumberPage(
-//                           email: widget.email,
-//                           name: widget.name,
-//                           id: idController.text,
-//                           pwd: pwdController.text,
-//                           pwdcheck: pwdCheckController.text)))
-//                   : null;
-//             },
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
