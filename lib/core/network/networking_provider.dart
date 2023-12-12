16e8f525323be788493e8f8601b65187bcdf6715
yoochanhong <chanhongy6@naver.com>
2023-12-12 15:54:09 +0900

    ⚡️ :: Jwt Interceptor 의존성 주입

--- lib/core/network/networking_provider.dart
+++ lib/core/network/networking_provider.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mukgen_flutter_v1/core/jwt_store/jwt_store_provider.dart';
 import 'package:mukgen_flutter_v1/core/network/jwt_interceptor.dart';
 import 'package:mukgen_flutter_v1/core/network/networking.dart';
 import 'package:mukgen_flutter_v1/core/network/networking_impl.dart';
 
-final networkingProvider = Provider<Networking>(
-    (ref) => NetworkingImpl()..interceptor.add(JwtInterceptor()));
+final networkingProvider = Provider<Networking>((ref) => NetworkingImpl()
+  ..interceptor.add(JwtInterceptor(jwtStore: ref.watch(jwtStoreProvider))));
