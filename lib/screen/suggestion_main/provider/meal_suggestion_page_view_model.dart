49a7bf1096f10a83a20a786e733e9f96c88b15dc
yoochanhong <chanhongy6@naver.com>
2023-12-14 00:44:01 +0900

    ✨ :: 급식 건의 싫어요 추가 기능 구현

--- lib/screen/suggestion_main/provider/meal_suggestion_page_view_model.dart
+++ lib/screen/suggestion_main/provider/meal_suggestion_page_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mukgen_flutter_v1/core/network/result.dart' as r;
+import 'package:mukgen_flutter_v1/domain/entity/meal_suggestion/meal_suggestion_entity.dart';
 import 'package:mukgen_flutter_v1/domain/use_case/meal_suggestion/add_meal_suggestion_dislike_use_case.dart';
 import 'package:mukgen_flutter_v1/domain/use_case/meal_suggestion/add_meal_suggestion_like_use_case.dart';
 import 'package:mukgen_flutter_v1/domain/use_case/meal_suggestion/create_meal_suggestion_use_case.dart';
@@ -33,13 +34,39 @@ class MealSuggestionPageViewModel
         super(MealSuggestionPageState.initial());
 
   Future<void> readAllMealSuggestion() async {
-    state = MealSuggestionPageState.loading();
+    state = state.copyWith(state: MealSuggestionPageStateEnum.loading);
     final res = await _readAllMealSuggestionUseCase.execute();
     switch (res) {
       case r.Success(value: final value):
-        state = MealSuggestionPageState.success(mealSuggestions: value);
+        state = state.copyWith(
+            state: MealSuggestionPageStateEnum.success, mealSuggestions: value);
       case r.Failure(exception: final e):
-        state = MealSuggestionPageState.failure(errorMessage: e.toString());
+        state = state.copyWith(
+            state: MealSuggestionPageStateEnum.failure, mealSuggestions: []);
+    }
+  }
+
+  Future<void> addMealSuggestionDislike({required int mealSuggestionId}) async {
+    final res = await _addMealSuggestionDislikeUseCase.execute(
+        mealSuggestionId: mealSuggestionId);
+    switch (res) {
+      case r.Success():
+        state = state.copyWith(
+            state: MealSuggestionPageStateEnum.success,
+            mealSuggestions: state.mealSuggestions
+                .map((e) => e.id == mealSuggestionId
+                    ? MealSuggestionEntity(
+                        id: e.id,
+                        likeCount: e.likeCount,
+                        dislikeCount: e.dislikeCount + 1,
+                        content: e.content,
+                        checked: e.checked)
+                    : e)
+                .toList());
+      case r.Failure(exception: final e):
+        state = state.copyWith(
+            state: MealSuggestionPageStateEnum.failure,
+            failMessage: e.toString());
     }
   }
 }
